f44dc03b78114d32114dcab31de58c23a5b84f48
Bethany <krullbethany@gmail.com>
2019-01-17 13:11:15 -0600

    I made a hello world program.
    It prints my name and major.

---
+++ hello.c
@@ -0,0 +1,16 @@
+/*
+* Authors: Bethany Krull and Jacob French 
+* Date: 01/17/19
+*
+* Program to print my name
+*/
+
+#include<stdlib.h>
+#include<stdo.h>
+
+int main(int argc, char **argv)
+{
+	printf("Bethany Krull!\n");
+	printf("My major is Computer Engineering.");
+	return 0;
+}
